b44e02c0692ba207151c544280d4cd18f0350470
Unknown <MartinSStewart@gmail.com>
2018-05-12 14:53:00 +0200

    Added JS to remove 000webhost banner.
    I'd tolerate it if it weren't for it blocking mouse clicks over the entire bottom edge of the window.

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,6 +2,19 @@ import './main.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
+document.addEventListener("DOMContentLoaded", function(event) { 
+        var elements = document.querySelectorAll( 'body > *' );
+        for (var i = elements.length - 1; i >= 0; i--)
+        {
+            var element = elements[i];
+            if (element.localName === "div" && element.id === "")
+            {
+                element.remove();
+                break;
+            }
+        }
+    });
+
 Main.embed(document.getElementById('root'));
 
 registerServiceWorker();
